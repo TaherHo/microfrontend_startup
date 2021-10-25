0ee7d5338d717c41362fdc1bb26c9c815d9cdd45
TaHo <hoseininejad@gmail.com>
2021-10-25 23:54:02 +0200

    :tada: Erstelle Micro Frontend Skeleton

---
+++ container/config/webpack.dev.js
@@ -0,0 +1,27 @@
+const { merge } = require("webpack-merge");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const commonWebpack = require("./webpack.common");
+
+const devConfig = {
+  mode: "development",
+  devServer: {
+    port: 8080,
+    historyApiFallback: {
+      index: "index.html",
+    },
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "contanier",
+      remotes: {
+        marketing: "marketing@http://localhost:8081/remoteEntry.js",
+      },
+    }),
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+};
+
+module.exports = merge(commonWebpack, devConfig);
---
+++ container/src/App.js
@@ -0,0 +1,12 @@
+import React, { useEffect, useRef } from "react";
+import MarketingApp from "./components/MarketingApp";
+
+export default () => {
+  return (
+    <div>
+      <h1>Hi from contabier</h1>
+      <hr />
+      <MarketingApp />
+    </div>
+  );
+};
